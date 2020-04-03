5e838ea5a2b9396b07600b6f7efa217182ab8784
Thomas Foster <thomasvfoster@gmail.com>
2020-04-03 14:16:27 +0100

    remove stuff we don't need

--- public/app.js
+++ public/app.js
@@ -2,37 +2,11 @@
   const messages = document.querySelector('#messages');
   const wsButton = document.querySelector('#wsButton');
   const wsSendButton = document.querySelector('#wsSendButton');
-  const logout = document.querySelector('#logout');
-  const login = document.querySelector('#login');
 
   function showMessage(message) {
     messages.textContent += `\n${message}`;
     messages.scrollTop = messages.scrollHeight;
   }
-
-  function handleResponse(response) {
-    return response.ok
-      ? response.json().then((data) => JSON.stringify(data, null, 2))
-      : Promise.reject(new Error('Unexpected response'));
-  }
-
-  login.onclick = function() {
-    fetch('/login', { method: 'POST', credentials: 'same-origin' })
-      .then(handleResponse)
-      .then(showMessage)
-      .catch(function(err) {
-        showMessage(err.message);
-      });
-  };
-
-  logout.onclick = function() {
-    fetch('/logout', { method: 'DELETE', credentials: 'same-origin' })
-      .then(handleResponse)
-      .then(showMessage)
-      .catch(function(err) {
-        showMessage(err.message);
-      });
-  };
 
   let ws;
 
