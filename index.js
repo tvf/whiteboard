df54a342d3e16f6e85c19da735668d893b20ad5b
Thomas Foster <thomasvfoster@gmail.com>
2020-04-03 15:18:23 +0100

    add a canvas and start sending messages

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const uuid = require('uuid');
 const WebSocket = require('ws');
 
 const app = express();
-const map = new Map();
+const clientmap = new Map();
+
+const drawlist = [];
 
 //
 // Serve static files from the 'public' folder.
@@ -24,16 +26,25 @@ server.on('upgrade', function(request, socket, head) {
 });
 
 wss.on('connection', function(ws, request) {
+
   const userId = uuid.v4();
 
-  map.set(userId, ws);
+  clientmap.set(userId, ws);
 
   ws.on('message', function(message) {
     console.log(`Received message ${message} from user ${userId}`);
+
+    let artist_location = {
+      type: "artist location",
+      name: "Thomas",
+      position: [20, 40],
+    };
+
+    ws.send(JSON.stringify(artist_location));
   });
 
   ws.on('close', function() {
-    map.delete(userId);
+    clientmap.delete(userId);
   });
 });
 
--- public/app.js
+++ public/app.js
@@ -2,6 +2,11 @@
   const messages = document.querySelector('#messages');
   const wsButton = document.querySelector('#wsButton');
   const wsSendButton = document.querySelector('#wsSendButton');
+  const whiteboard = document.querySelector('#whiteboard');
+  const whiteboard_ctx = whiteboard.getContext('2d');
+
+  const artist_locations = new Map();
+  let dirty = true;
 
   function showMessage(message) {
     messages.textContent += `\n${message}`;
@@ -17,15 +22,32 @@
     }
 
     ws = new WebSocket(`ws://${location.host}`);
+
     ws.onerror = function() {
       showMessage('WebSocket error');
     };
+
     ws.onopen = function() {
       showMessage('WebSocket connection established');
     };
+
     ws.onclose = function() {
       showMessage('WebSocket connection closed');
       ws = null;
+    };
+
+    ws.onmessage = function(message) {
+      let content = JSON.parse(message.data);
+
+      if (content.type === 'artist location') {
+        if (content.hasOwnProperty('position')) {
+          artist_locations.set(content.name, content.position);
+        } else {
+          artist_locations.delete(content.name);
+        }
+
+        dirty = true;
+      }
     };
   };
 
@@ -35,7 +57,34 @@
       return;
     }
 
-    ws.send('Hello World!');
+    let penstrokes = [{
+      start: [0, 0, 20],
+      end: [1000, 0, 30],
+      colour: [127, 127, 127]
+    }];
+
+    ws.send(JSON.stringify(penstrokes));
+
     showMessage('Sent "Hello World!"');
   };
+
+  function render(now) {
+
+    if (dirty) {
+
+      whiteboard_ctx.fillStyle = 'rgb(127, 127, 127)';
+      whiteboard_ctx.fillRect(0,0,640,480);
+
+      whiteboard_ctx.fillStyle = 'rgb(255, 0, 0)';
+      artist_locations.forEach((position) => {
+        whiteboard_ctx.fillRect(position[0], position[1], 1, 1);
+      }); // TODO text?
+
+      dirty = false;
+    }
+    
+    requestAnimationFrame(render);
+  }
+
+  requestAnimationFrame(render);
 })();
